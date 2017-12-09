97c1411bb5f27e13e5d3caea381a6312dec1fbf3
anthonyn14 <anthonynguyen567@gmail.com>
2017-12-09 11:20:02 -0800

    Update rubik.js

--- rubik.js
+++ rubik.js
@@ -664,8 +664,9 @@ YUI.add('rubik', function (Y) {
             plane.addClass(m.slice +'-'+ m.rotate);
 
 
-              this._behaviorTree();// wasnt sure how to test behaviortree function
+            //  this._behaviorTree();// wasnt sure how to test behaviortree function
               // just put it in domovment function for now. -anthony december saturday
+              // move to _updatePlaneList
         },
         _attachToPlane:function (list) {
             this._plane.setContent(list);
@@ -844,6 +845,7 @@ YUI.add('rubik', function (Y) {
             }
             //console.log(new_list);
             //return new_list;
+            this._behaviorTree(); // added by anthony 11:17am december 9
         },
         _getKeyByValue: function(object, value) {
             return Object.keys(object).find(key => object[key] === value);
